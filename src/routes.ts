711c7c3f91e1153af9ace99b5c30145a9f52cff7
kolynzb <collinsbenda360@gmail.com>
2023-07-08 09:32:29 +0300

    feat: create route description

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+export default (app: express.Application): void => {
+  // app.use('/api/v1/users', userRoute);
+  // app.use('/api/v1/avatars', avatarRoute);
+  // app.use('/api/v1/blog/articles', articleRoute);
+  // app.use('/api/v1/blog/likes', articleLikeRoute);
+  // app.use('/api/v1/blog/follows', articleFollowRoute);
+  // app.use('/api/v1/blog/categories', articleCategoryRoute);
+};
