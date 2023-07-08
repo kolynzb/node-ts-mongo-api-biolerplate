9c757aa68dd64c37cc44550b996e7eb3dbced38e
kolynzb <collinsbenda360@gmail.com>
2023-07-08 09:32:06 +0300

    chore: create sample test

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,10 +1,7 @@
 // sum.ts
-function sum(a: number, b: number): number {
-    return a + b;
-  }
-  
-  
-  test('sum function should add two numbers correctly', () => {
-    const result = sum(2, 3);
-    expect(result).toBe(5);
-  });
+const sum = (a: number, b: number): number  => a + b;
+
+test('sum function should add two numbers correctly', () => {
+  const result = sum(2, 3);
+  expect(result).toBe(5);
+});
