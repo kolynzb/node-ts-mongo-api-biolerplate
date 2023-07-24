909f61e786eb5b5e584345bd003f64619f0a8032
kolynzb <collinsbenda360@gmail.com>
2023-07-24 12:59:00 +0300

    feat: create base auth schema

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const RegisterUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const emailSchema = z.string().email();
+export const passwordSchema = z
+  .string()
+  .min(8, 'Password must be at least 8 characters long')
+  .max(100, 'Password must be at most 100 characters long')
+  .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]/, {
+    message: 'Password must contain at least one letter, one number, and one special character',
+  });
+
+export const LoginUserSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const RefreshAccessTokenSchema = z.object({
+  refreshToken: z.string(),
+});
+
+export const confirmEmailSchema = z.object({
+  token: z.string(),
+});
+
+export const googleAuthSchema = z.object({
+  idToken: z.string(),
+});
