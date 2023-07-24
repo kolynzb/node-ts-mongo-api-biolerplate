66a05085fc62e6d90243225c7bd8213d9911d435
kolynzb <collinsbenda360@gmail.com>
2023-07-24 12:47:26 +0300

    feat: create app settings

---
+++ src/config/settings.config.ts
@@ -0,0 +1,12 @@
+const appSettings = {
+  appName: 'Node Js Mongo Boilerplate',
+  generalUserName: 'Nodeling',
+  author: { name: 'Kolo' },
+  emails: {
+    name: 'Kolo',
+    title: 'Maintainer',
+    from: 'collinsbenda360@gmail.com',
+  },
+};
+
+export default appSettings;
