8c40f3466d61a2975020019044afd4a86c66db78
kolynzb <collinsbenda360@gmail.com>
2023-08-10 12:50:29 +0300

    feat: format routes

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -10,14 +10,14 @@ const authController = new AuthController();
 router.post('/login', authController.login.bind(authController));
 router.post('/register', authController.register.bind(authController));
 
-router.post('/forgotPassword', authController.forgotPassword.bind(authController));
-router.patch('/resetPassword', authController.resetPassword.bind(authController));
-router.post('/resendVerificationEmail', authController.resendVerificationEmail.bind(authController));
-router.get('/verifyEmail/:token', authController.VerifyEmail.bind(authController));
+router.post('/forgot-password', authController.forgotPassword.bind(authController));
+router.patch('/reset-password', authController.resetPassword.bind(authController));
+router.post('/resend-verification-email', authController.resendVerificationEmail.bind(authController));
+router.get('/verify-email/:token', authController.VerifyEmail.bind(authController));
 
 router.use(protect);
 router.post('/refresh', authController.refresh.bind(authController));
-router.patch('/changePassword', authController.changePassword.bind(authController));
+router.patch('/change-password', authController.changePassword.bind(authController));
 router.delete('/logout', authController.logout.bind(authController));
 
 export default router;
