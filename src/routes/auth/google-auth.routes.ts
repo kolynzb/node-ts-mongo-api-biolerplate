d780104f5fbab6db3f945dd7e20f492b04b815e4
kolynzb <collinsbenda360@gmail.com>
2023-08-10 13:44:39 +0300

    feat: create google authentication routes

--- src/routes/auth/google-auth.routes.ts
+++ src/routes/auth/google-auth.routes.ts
@@ -1,5 +1,12 @@
+import GoogleAuthController from '@controllers/auth/google-auth.controller';
 import { Router } from 'express';
 
 const router = Router();
+const googleAuthController = new GoogleAuthController();
+
+router.get('/url', googleAuthController.googleAuthUrl);
+
+// Callback route for Google OAuth
+router.get('/callback', googleAuthController.googleAuthCallback);
 
 export default router;
